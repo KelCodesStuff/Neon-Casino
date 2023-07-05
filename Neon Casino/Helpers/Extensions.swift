3a9ede971c5ea0a5f7d902c6fb23dbc3e95e3aa4
Kelvin Reid <codesmithk@gmail.com>
2023-07-05 15:07:15 -0400

    Adding helper files

---
+++ Neon Casino/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Slot Machine
+//
+//  Created by Kelvin Reid on 7/2/23.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
---
+++ Neon Casino/Helpers/Modifiers.swift
@@ -0,0 +1,95 @@
+//
+//  Modifiers.swift
+//  Slot Machine
+//
+//  Created by Kelvin Reid on 7/2/23.
+//
+
+import SwiftUI
+
+// MARK: - Reset & info button mod
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .accentColor(Color.white)
+            .padding()
+    }
+}
+
+// MARK: - Jackpot number mod
+struct JackpotLabelModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.system(size: 20, weight: .heavy, design: .rounded))
+    }
+}
+
+// MARK: - Score number mod
+struct ScoreNumberModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.system(size: 20, weight: .heavy, design: .rounded))
+    }
+}
+
+// MARK: - Score label mod
+struct ScoreLabelModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+}
+
+// MARK: - Score capsule mod
+struct ScoreCapsuleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.vertical, 10)
+            .padding(.horizontal, 16)
+            .background(
+                Capsule()
+                    .foregroundColor(Color("ColorBlackTransparent")))
+    }
+}
+
+// MARK: - Reel image mod
+struct ReelImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .scaledToFit()
+            .frame(minWidth: 110, idealWidth: 110, maxWidth: 110, alignment: .center)
+    }
+}
+
+// MARK: - Symbol image mod
+struct SymbolImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .scaledToFit()
+            .frame(minWidth: 70, idealWidth: 70, maxWidth: 70, alignment: .center)
+    }
+}
+
+// MARK: - Bet button mod
+struct BetButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 50, height: 50)
+    }
+}
+
+// MARK: - Spin button mod
+struct SpinButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+    }
+}
+
+
