93917f262f40007e6608dd67e1d65eb6d2ca5fad
KelCodesStuff <codesmithk@gmail.com>
2025-08-18 18:37:57 -0400

    Add spacing for readability in SlotMachinePage

--- Tests/UITests/Neon-CasinoUITests.swift
+++ Tests/UITests/Neon-CasinoUITests.swift
@@ -16,8 +16,10 @@ struct SlotMachinePage {
     let app: XCUIApplication
 
     var spinButton: XCUIElement { app.buttons["spinButton"] }
+    
     var moneyLabel: XCUIElement { app.staticTexts["moneyValueLabel"] }
     var jackpotLabel: XCUIElement { app.staticTexts["jackpotValueLabel"] }
+    
     var jackpotAlert: XCUIElement { app.alerts["Jackpot!"] }
     var winAlert: XCUIElement { app.alerts["Congratulations!"] }
     var gameOverAlert: XCUIElement { app.alerts["Game Over"] }
